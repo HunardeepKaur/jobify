24d93048dac3bd6f4513aee0a1aef442bc60128e
HunardeepKaur <hunar192005@gmail.com>
2026-01-14 21:38:48 +0530

    latest

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
+console.log("API KEY FROM ENV:", process.env.REACT_APP_FIREBASE_API_KEY);
 
 
 const app = initializeApp(firebaseConfig);
